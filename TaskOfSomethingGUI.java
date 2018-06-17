5877c4a771e062d6fe0b05651a90e84d6e418f2f
Swierszcz32 <adi.zwierzchowski@gmail.com>
2018-06-18 01:41:35 +0200

    Add GroupLayout 2 version

--- TaskOfSomethingGUI.java
+++ TaskOfSomethingGUI.java
@@ -1,6 +1,8 @@
 package pl.java.workExperience;
 
 import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
+
 import java.awt.*;
 
 public class TaskOfSomethingGUI extends JFrame{
@@ -11,7 +13,8 @@ public class TaskOfSomethingGUI extends JFrame{
 		this.setVisible(true);
 		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 		settingTheSizeOfTheFrame();
-		AddComponents();
+		addComponents();
+		pack();
 	}
 	public void settingTheSizeOfTheFrame() {
 		int height = Toolkit.getDefaultToolkit().getScreenSize().height;
@@ -19,25 +22,79 @@ public class TaskOfSomethingGUI extends JFrame{
 		this.setBounds((width-(width/2))/2, (height-(height/2))/2, width/2, height/2);
 	}
 	
-	JLabel label = new JLabel("pustka");
-	JPanel panel = new JPanel();
+	JLabel label1 = new JLabel(""+TaskOfSomething.FibR(10));
+	JLabel label2 = new JLabel("recursive solution");
+	JLabel label3 = new JLabel(""+TaskOfSomething.FibI(10));
+	JLabel label4 = new JLabel("iterative solution");
+	JLabel label5 = new JLabel("Fibonacci sequence - two solutions");
+	JPanel panel = new JPanel(new FlowLayout());
 	GroupLayout layout = new GroupLayout(getContentPane());
 	
-	public void AddComponents() {
-		layout.setHorizontalGroup(
-				layout.createSequentialGroup()
-				.addGroup(
-						layout.createParallelGroup()
-						.addComponent(label, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						)
-				);
-		layout.setVerticalGroup(
-				layout.createSequentialGroup()
-				.addGroup(
-						layout.createParallelGroup()
-						.addComponent(label, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						)
-				);
+	public void addComponents() {
+		this.setLayout(layout);
+
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+		
+//		layout.setHorizontalGroup(
+//				layout.createSequentialGroup()
+//				.addGroup(
+//						layout.createParallelGroup()
+//						.addComponent(label2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+//						.addComponent(label1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+//						)
+//				.addGroup(
+//						layout.createParallelGroup()
+//						.addComponent(label4, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+//						.addComponent(label3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+//						)
+//				);
+//		layout.setVerticalGroup(
+//				layout.createSequentialGroup()
+//				.addGroup(
+//						layout.createParallelGroup()
+//						.addComponent(label2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+//						.addComponent(label4, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+//						)
+//				.addGroup(
+//						layout.createParallelGroup()
+//						.addComponent(label1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+//						.addComponent(label3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+//						)
+//				);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(Alignment.LEADING)
+                .addGroup(Alignment.TRAILING, layout.createSequentialGroup()
+                    .addGap(104, 104, 104)
+                    .addGroup(layout.createParallelGroup(Alignment.TRAILING)
+                        .addComponent(label5, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGroup(layout.createParallelGroup(Alignment.LEADING, false)
+                                .addComponent(label2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(label1, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE))
+                            .addGap(18, 18, 18)
+                            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                .addComponent(label4, GroupLayout.DEFAULT_SIZE, 92, Short.MAX_VALUE)
+                                .addComponent(label3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                    .addGap(96, 96, 96))
+            );
+            layout.setVerticalGroup(
+                layout.createParallelGroup(Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(20, 20, 20)
+                    .addComponent(label5, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addGroup(layout.createParallelGroup(Alignment.BASELINE)
+                        .addComponent(label2, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(label4, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
+                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                    .addGroup(layout.createParallelGroup(Alignment.BASELINE)
+                        .addComponent(label1, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(label3, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
+                    .addContainerGap(143, Short.MAX_VALUE))
+            );
+
+            pack();
 	}
 	
 
